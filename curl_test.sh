0f88d33e8899744a1fafccc102a68b5b0d514576
WeiDe Su <weide_su@mitake.com.tw>
2020-05-28 18:44:29 +0800

    Add const

---
+++ curl_test.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+T_OK='47e9b9c8ffb7cc4310f72189ef9d5cc9cf56d1c286083f5e41118c1f1b91c273'
+T_VOIP='da02c395db49b9918706b34a7dfba7cb670120b6d254be41572c1f960498848a'
+T_FAIL='05599d8ea99c2c924c639c13873393d79abbf29453db952933ed11adeba0b8d8'
+
+
+/usr/bin/curl -1kv --http2 --cert ./apns_test.p12:86136982 \
+-H ":method : POST'" \
+-H "path : /3/device/$T_OK" \
+-H "apns-push-type : alert" \
+-H "apns-topic : com.mitake.mitakeeim" \
+-d "{\"aps\":{\"alert\":\"Hello\"}}" \
+-X POST https://api.push.apple.com
+#-H "path:/3/device/47e9b9c8ffb7cc43" \ 
--- executor.go
+++ executor.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"tw.com.wd/push/apns/cert"
+	"tw.com.wd/push/apns/notification"
 )
 
 func main() {
@@ -11,4 +12,10 @@ func main() {
 	var filePath = "apns_test.p12"
 
 	cert.ReadP12FromFile(filePath, "86136982")
+
+	fmt.Printf("\n")
+
+	fmt.Printf("Token OK: %v\n", notification.Token_OK)
+	fmt.Printf("Token Fail: %v\n", notification.Token_Fail)
+	fmt.Printf("Token VOIP: %v\n", notification.Token_VOIP)
 }
