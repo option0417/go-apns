6bb2a02b86fc50fc836b877e888f77e054ce090b
WeiDe Su <weide_su@mitake.com.tw>
2020-07-23 15:33:02 +0800

    Update push-token

--- common/const.go
+++ common/const.go
@@ -4,7 +4,7 @@ const (
 	APNS_DEVELOPMENT_SERVER = "api.sandbox.push.apple.com"
 	APNS_PRODUCTION_SERVER  = "api.push.apple.com"
 
-	Token_OK   = "47e9b9c8ffb7cc4310f72189ef9d5cc9cf56d1c286083f5e41118c1f1b91c273"
+	Token_OK   = "0b02a92dc35d2bd297988b75ead46c356ff8aceb589ee032773eff85d9c1a952"
 	Token_Fail = "05599d8ea99c2c924c639c13873393d79abbf29453db952933ed11adeba0b8d8"
 	Token_VOIP = "da02c395db49b9918706b34a7dfba7cb670120b6d254be41572c1f960498848a"
 )
--- example.go
+++ example.go
@@ -12,7 +12,7 @@ func main() {
 	var filePath = "apns_test.p12"
 	tlsCert, err := cert.ReadP12FromFile(filePath, "86136982")
 
-	var token = "47e9b9c8ffb7cc4310f72189ef9d5cc9cf56d1c286083f5e41118c1f1b91c273"
+	var token = "0b02a92dc35d2bd297988b75ead46c356ff8aceb589ee032773eff85d9c1a952"
 	var topic = "com.mitake.mitakeeim"
 	var payload = "{\"aps\":{\"alert\":\"Hello\"}}"
 
