956dbb061e12e5e9b2fbdc0283fc45304d6da2d6
option0417 <option0417@gmail.com>
2020-07-28 23:56:28 +0800

    Code refactor

--- common/const.go
+++ common/const.go
@@ -1,10 +1,16 @@
 package common
 
+import "fmt"
+
 const (
 	APNS_DEVELOPMENT_SERVER = "api.sandbox.push.apple.com"
 	APNS_PRODUCTION_SERVER  = "api.push.apple.com"
 
+	TOPIC = "com.mitake.mitakeeim"
+
 	Token_OK   = "0b02a92dc35d2bd297988b75ead46c356ff8aceb589ee032773eff85d9c1a952"
 	Token_Fail = "05599d8ea99c2c924c639c13873393d79abbf29453db952933ed11adeba0b8d8"
 	Token_VOIP = "da02c395db49b9918706b34a7dfba7cb670120b6d254be41572c1f960498848a"
+
+	PAYLOAD_A = "{\"aps\":{\"alert\":\"Hello\"}}"
 )
--- executor.go
+++ executor.go
@@ -2,25 +2,31 @@ package main
 
 import (
 	"fmt"
-	"tw.com.wd/push/apns/cert"
+	//"tw.com.wd/push/apns/cert"
+	"tw.com.wd/push/apns/common"
 	//"tw.com.wd/push/apns/push"
 )
 
 func main() {
-	var filePath = "apns_test.p12"
-	tlsCert, err := cert.ReadP12FromFile(filePath, "86136982")
+	//var filePath = "apns_test.p12"
+	//tlsCert, err := cert.ReadP12FromFile(filePath, "86136982")
 
-	if err != nil {
-		fmt.Println("Error: %v\n", err)
-		return
-	}
+	/*
+		if err != nil {
+			fmt.Println("Error: %v\n", err)
+			return
+		}
+	*/
 
-	var token = "47e9b9c8ffb7cc4310f72189ef9d5cc9cf56d1c286083f5e41118c1f1b91c273"
-	var topic = "com.mitake.mitakeeim"
-	var payload = "{\"aps\":{\"alert\":\"Hello\"}}"
-	var payloadMap = make(map[string]string)
-	payloadMap["key1"] = "value1"
-	payloadMap["key2"] = "value2"
+	/*
+		var payloadMap = make(map[string]string)
+		payloadMap["key1"] = "value1"
+		payloadMap["key2"] = "value2"
+	*/
+
+	fmt.Printf("Token: %v\n", common.Token_OK)
+	fmt.Printf("Topic: %v\n", common.TOPIC)
+	fmt.Printf("Payload: %v\n", common.PAYLOAD_A)
 
 	// Build payload
 	// payload.PayloadBuilder().tokens([1]string{token}).topic(topic).payload(
