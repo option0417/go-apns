d22ef97ed305a349ceeae6f933c05fa1890084a4
WeiDe Su <weide_su@mitake.com.tw>
2020-07-28 20:28:05 +0800

    Update

--- executor.go
+++ executor.go
@@ -18,6 +18,16 @@ func main() {
 	var token = "47e9b9c8ffb7cc4310f72189ef9d5cc9cf56d1c286083f5e41118c1f1b91c273"
 	var topic = "com.mitake.mitakeeim"
 	var payload = "{\"aps\":{\"alert\":\"Hello\"}}"
+	var payloadMap = make(map[string]string)
+	payloadMap["key1"] = "value1"
+	payloadMap["key2"] = "value2"
 
+	// Build payload
+	// payload.PayloadBuilder().tokens([1]string{token}).topic(topic).payload(
+
+	// Build PushClient
 	//push.BuildPushClient(token, topic, payload, tlsCert)
+
+	// Do Push
+
 }
